3d4919892fb1eae1608e1a6377038876fdff78e2
neerajmayur19 <neeraj.mayur.999@gmail.com>
2024-04-10 12:38:03 +0530

    Changed the name

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
 
     // Send the response body "Hello World!"
-    res.end('Hello World!\n');
+    res.end('Hello Neeraj!\n');
 });
 
 // Start listening on the specified port
